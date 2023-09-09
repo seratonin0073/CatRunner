ea19d12e6f61760047a425e44c7a1677c771e5be
seratonin0073 <mizko2000y@gmail.com>
2023-09-09 19:38:45 +0300

    Lesson2

--- Assets/Code Base/PlayerController.cs
+++ Assets/Code Base/PlayerController.cs
@@ -1,18 +1,18 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-
+    private Vector2 direction; //поле яке зберігає напрям руху
+    private float speed = 3.2f; //поле яке зберігає швидкість руху
     void Start()
     {
-        
+        Debug.Log("Hello Unity!");
     }
-
 
     void Update()
     {
-        
+        direction.x = Input.GetAxis("Horizontal"); // отримуємо значення з клавіш керування (Horizontal вісь)
+        direction.y = Input.GetAxis("Vertical"); // отримуємо значення з клавіш керування (Vertical вісь)
+        transform.Translate(direction.normalized * speed * Time.deltaTime); //метод переміщення об'єкту
     }
 }
